38a157cbb87f314e01e16973eea7556fbe9eaee6
manoti3 <hillarymanoti0@gmail.com>
2024-03-24 16:46:29 +0300

    completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,20 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        # perform depth-first traversal
+        stack = [self.root]
+
+        while stack:
+            node = stack.pop()
+
+            # check if the current node matches the desired ID
+            if node['id'] == id:
+                return node
+            
+            # Add children to the traversal
+            stack.extend(node['children'])
+
+        # return None, if no matching element is found
+        return None
